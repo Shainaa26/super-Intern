8f8904aa2230c1435731b4eea2bcfb3bf22e4c71
Shainaa26 <96491126+Shainaa26@users.noreply.github.com>
2022-11-21 21:10:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,94 @@
+fetch("https://random-data-api.com/api/users/random_user?size=100").then((data)=>{
+   return data.json();
+
+} ). then((objectData)=> {
+   console.log(objectData[0].title);
+   let tableData="";
+   objectData.map((values)=> {
+      tableData+=`  
+        <tr>
+      <td> ${values.username} </td>
+      <td>${values.email}</td>
+      <td>${values.gender}</td>
+      <td>${values.address.city}</td>
+      <td>${values.subscription.status}</td>
+  </tr>`;
+   });
+   document.getElementById("table_body").innerHTML= tableData;
+} )   
+
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName('tr');
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td){
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function myFunction2() {
+   var input, filter, table, tr, td, i, txtValue;
+   input = document.getElementById('myInput2');
+   filter = input.value.toUpperCase();
+   table = document.getElementById("myTable");
+   tr = table.getElementsByTagName('tr');
+   for (i = 0; i < tr.length; i++) {
+     td = tr[i].getElementsByTagName("td")[1];
+     if (td){
+       txtValue = td.textContent || td.innerText;
+       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         tr[i].style.display = "";
+       } else {
+         tr[i].style.display = "none";
+       }
+     }
+   }
+ }
+
+ function myFunction3() {
+   var input, filter, table, tr, td, i, txtValue;
+   input = document.getElementById('myFilter');
+   filter = input.value.toUpperCase();
+   table = document.getElementById("myTable");
+   tr = table.getElementsByTagName('tr');
+   for (i = 0; i < tr.length; i++) {
+     td = tr[i].getElementsByTagName("td")[3];
+     if (td){
+       txtValue = td.textContent || td.innerText;
+       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         tr[i].style.display = "";
+       } else {
+         tr[i].style.display = "none";
+       }
+     }
+   }
+ }
+      
+ function myFunction4() {
+   var input, filter, table, tr, td, i, txtValue;
+   input = document.getElementById('myFilter2');
+   filter = input.value.toUpperCase();
+   table = document.getElementById("myTable");
+   tr = table.getElementsByTagName('tr');
+   for (i = 0; i < tr.length; i++) {
+     td = tr[i].getElementsByTagName("td")[4];
+     if (td){
+       txtValue = td.textContent || td.innerText;
+       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         tr[i].style.display = "";
+       } else {
+         tr[i].style.display = "none";
+       }
+     }
+   }
+ }
